e7435603a6ea808c9b4ea76da8dfe0617c729005
vijaydevguy <vijayakumarjob01@gmail.com>
2026-08-18 22:55:54 +0530

    jwt

--- src/models/user.js
+++ src/models/user.js
@@ -77,13 +77,13 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 userSchema.methods.getJWT = async function () {
   const user = this;
 
-  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+  const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
@@ -96,7 +96,7 @@ userSchema.methods.validatePassword = async function (passwordInputByUser) {
 
   const isPasswordValid = await bcrypt.compare(
     passwordInputByUser,
-    passwordHash
+    passwordHash,
   );
 
   return isPasswordValid;
